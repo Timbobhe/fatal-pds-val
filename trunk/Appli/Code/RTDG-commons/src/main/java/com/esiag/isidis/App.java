36f1b9c6054c980cfccc27df28b5c9e9a3f7e2c8
saoudi.hamza00@gmail.com <saoudi.hamza00@gmail.com@64f3a4ae-8618-586d-75de-2c8adf00f9b5>
2012-11-21 13:39:00 +0000

    composant RTDG-commons

---
+++ trunk/Appli/Code/RTDG-commons/src/main/java/com/esiag/isidis/App.java
@@ -0,0 +1,13 @@
+package com.esiag.isidis;
+
+/**
+ * Hello world!
+ *  HSI
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
